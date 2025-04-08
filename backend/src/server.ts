adb305c20ebc6d301c33c1906704f6e81d19db4f
YaseerM16 <yaseerbscit@gmail.com>
2025-04-08 12:58:43 +0530

    added backend render link

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ app.use(morgan('dev'))
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://wholesale-delivery-management-app.vercel.app"],
     credentials: true,
     methods: "GET,POST,PUT,DELETE,PATCH",
 }));
